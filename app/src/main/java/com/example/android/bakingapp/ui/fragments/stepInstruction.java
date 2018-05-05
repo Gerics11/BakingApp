70e62785da650a877fcb61ce0174d5ba3b78ec5b
Gerics11 <feher.gergo9@gmail.com>
2018-05-05 19:04:06 +0200

    initial commit.

--- app/src/main/java/com/example/android/bakingapp/ui/fragments/stepInstruction.java
+++ app/src/main/java/com/example/android/bakingapp/ui/fragments/stepInstruction.java
@@ -1,54 +1,62 @@
 package com.example.android.bakingapp.ui.fragments;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.data.JsonData;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.Serializable;
+import java.util.Map;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link stepInstruction.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link stepInstruction#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class stepInstruction extends Fragment { //todo holds data of current step
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class StepInstruction extends Fragment implements ExoPlayer.EventListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String CODE_MAP = "hashmap";
+    private static final String CODE_PLAYBACK_POSITION = "playbackPosition";
 
-    private OnFragmentInteractionListener mListener;
+    private Map<String, String> step;
+    private SimpleExoPlayer exoPlayer;
+    private SimpleExoPlayerView videoView;
 
-    public stepInstruction() {
-        // Required empty public constructor
+    private StepInstructionClickListener stepInstructionCallBack;
+
+    public StepInstruction() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment stepInstruction.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static stepInstruction newInstance(String param1, String param2) {
-        stepInstruction fragment = new stepInstruction();
+    public static StepInstruction newInstance(Map<String, String> stepParam) {
+        StepInstruction fragment = new StepInstruction();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(CODE_MAP, (Serializable) stepParam);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,54 +65,165 @@ public class stepInstruction extends Fragment { //todo holds data of current ste
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            step = (Map<String, String>) getArguments().getSerializable(CODE_MAP);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_instruction, container, false);
+        View view = inflater.inflate(R.layout.fragment_step_instruction, container, false);
+        String instruction = step.get(JsonData.STEP_DESC);
+
+        TextView instructionTextView = view.findViewById(R.id.tv_instruction);
+        instructionTextView.setText(instruction);
+        //set button listeners
+        ImageView leftButton = view.findViewById(R.id.button_previous);
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLeftClicked();
+            }
+        });
+        ImageView rightButton = view.findViewById(R.id.button_next);
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRightClicked();
+            }
+        });
+
+        videoView = view.findViewById(R.id.video_player);
+
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(
+                getContext(),
+                new DefaultTrackSelector(),
+                new DefaultLoadControl());
+
+        //set up player
+        initializePlayer();
+        videoView.setUseController(true);
+        videoView.requestFocus();
+        videoView.setPlayer(exoPlayer);
+
+        if (savedInstanceState != null) {
+            exoPlayer.seekTo(savedInstanceState.getLong(CODE_PLAYBACK_POSITION));
+        }
+
+
+        exoPlayer.setPlayWhenReady(true);
+        exoPlayer.addListener(this);
+
+        if(!getResources().getBoolean(R.bool.isTablet) &&
+                getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            RelativeLayout instructionLayout = view.findViewById(R.id.instruction_layout);
+            instructionLayout.setVisibility(View.GONE);
+        }
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    //prepare player with media
+    private void initializePlayer() {
+        if (step.get(JsonData.STEP_VIDEO_URL).isEmpty()) {
+            videoView.setVisibility(View.GONE);
+        }
+        Uri uri = Uri.parse(step.get(JsonData.STEP_VIDEO_URL));
+        MediaSource mediaSource = buildMediaSource(uri);
+        LoopingMediaSource loopingSource = new LoopingMediaSource(mediaSource);
+        exoPlayer.prepare(loopingSource);
+    }
+
+    //extract data from uri
+    private MediaSource buildMediaSource(Uri uri) {
+        DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
+                Util.getUserAgent(getContext(),
+                "bakingApp"),
+                bandwidthMeterA);
+
+        return new ExtractorMediaSource(uri,
+                dataSourceFactory,
+                new DefaultExtractorsFactory(),
+                null,
+                null);
+    }
+
+    private void onLeftClicked() {
+        if (stepInstructionCallBack != null) {
+            stepInstructionCallBack.onVideoLeftClick();
+        }
+    }
+
+    private void onRightClicked() {
+        if (stepInstructionCallBack != null) {
+            stepInstructionCallBack.onVideoRightClick();
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof StepInstructionClickListener) {
+            stepInstructionCallBack = (StepInstructionClickListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement StepInstructionClickListener");
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onDestroyView() {
+        super.onDestroyView();
+        exoPlayer.removeListener(this);
+        exoPlayer.release();
+        stepInstructionCallBack = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(CODE_PLAYBACK_POSITION, exoPlayer.getCurrentPosition());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        exoPlayer.stop();
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == ExoPlayer.STATE_READY) {
+            videoView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            videoView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+    }
+
+
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+    }
+
+    public interface StepInstructionClickListener {
+        void onVideoLeftClick();
+        void onVideoRightClick();
     }
 }
